057645344333863faa353900e3923c4d42f8907c
NikitaMantia <nikitachilders@yahoo.com>
2022-07-24 20:50:13 -0500

    Added current location button

--- src/app.js
+++ src/app.js
@@ -35,18 +35,17 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
 function showTemperature(response) {
-  console.log(response.data);
   document.querySelector("#city-search").innerHTML = response.data.name;
   document.querySelector("#api-temp").innerHTML = Math.round(
     response.data.main.temp
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
   );
 }
 
 function search(event) {
   event.preventDefault();
-  // let cityElement = document.querySelector("#searched-city");
-  // let citySearch = document.querySelector("#city-input");
-  // cityElement.innerHTML = citySearch.value;
   let city = document.querySelector("#city-input").value;
   let apiKey = "97b9838032fcdf9880d37346b3190f7d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -54,3 +53,23 @@ function search(event) {
 }
 
 dateElement.innerHTML = formattedDate(currentTime);
+
+function showPosition(position) {
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+  let apiKey = "97b9838032fcdf9880d37346b3190f7d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function currentTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let head = (document.querySelector("#api-temp").innerHTML = Math.round(
+    response.data.main.temp
+  ));
+}
+
+navigator.geolocation.getCurrentPosition(showPosition);
+
+let currentLocationButton = document.querySelector("#button");
+currentLocationButton.addEventListener("click", showTemperature);
